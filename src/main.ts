2ee5d3dce1cd8b97b56974cd30473e5e13114835
palvinmuthesh <rrabbit2121@gmail.com>
2025-04-29 01:16:15 +0530

    today

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://brb-website.onrender.com'],
     credentials: true, // if you're using cookies/auth headers
   });
 
